8f17e0d1527e28762d8b68e12946acb29dea0dcc
ThaFartKnight <yveskerckhofs@gmail.com>
2017-02-14 20:19:10 +0100

    Add files via upload
    Added gold to the world generator

--- RTS - expiremental/src/com/caps/main/WorldGenerator.java
+++ RTS - expiremental/src/com/caps/main/WorldGenerator.java
@@ -1,5 +1,6 @@
 package com.caps.main;
 
+import com.caps.resource.Gold;
 import com.caps.resource.Wood;
 
 import java.io.*;
@@ -14,15 +15,18 @@ public class WorldGenerator
 		throws IOException {
 		GridCell[][] cells = grid.gridCells;
 		OpenSimplexNoise noise = new OpenSimplexNoise(i);
+		int j = 0;
 		for (int y = 0; y < HEIGHT; y++)
 		{
 			for (int x = 0; x < WIDTH; x++)
 			{
 				double value = noise.eval(x / FEATURE_SIZE, y / FEATURE_SIZE, 0.0);
 				if(value > 0.1 && value < 0.5 && y % 2 == 0 && !cells[x][y].isWall){
-					
 					handler.addObject(new Wood(x * 20, y * 20, ID.Resource, handler));
+				}else if(value < -0.1 && value > -0.14 && !cells[x][y].isWall && x % 2 == 0  && j < 30){
+					handler.addObject(new Gold(x * 20, y * 20, ID.Resource, handler));
 				}
+				
 			}
 		}
 	}
